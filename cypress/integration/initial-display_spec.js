5def7a09a6e3932bb7aeebd19bc5e2e51e44e30b
Cameron Aragon <caragon4695@gmail.com>
2021-03-09 10:27:24 -0700

    Test for the title and apperance of URLs

--- cypress/integration/initial-display_spec.js
+++ cypress/integration/initial-display_spec.js
@@ -2,15 +2,33 @@ describe('URL Shortener', () => {
     const baseUrl = 'http://localhost:3000/';
 
     beforeEach(() => {
-        cy.fixture('urlData.json')
+        cy
+        .fixture('urlData.json')
       .then(urls => {
-        cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
+        cy
+        .intercept('GET', 'http://localhost:3001/api/v1/urls', {
           body: urls
         })
       });
 
-      cy.visit(baseUrl);
+      cy
+      .visit(baseUrl);
     });
 
-    it('Should ')
+    it('Should display the page title of URL Shortener', () => {
+        cy
+        .get('h1').contains('URL Shortener')
+    })
+
+    it('Should display the existing shortened URLs', () => {
+        cy
+        .wait(500)
+        .get('section').children().its('length').should('eq', 2);
+    });
+
+    it('Should display the existing shortened URLs', () => {
+        cy
+        .wait(500)
+        .get('section').children().its('length').should('eq', 2);
+    });
 })
