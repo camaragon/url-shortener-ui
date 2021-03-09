4f83474b585ffa02227468d674eda7fed1007c53
Cameron Aragon <caragon4695@gmail.com>
2021-03-09 09:51:05 -0700

    Build POST fetch in API calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,4 +4,13 @@ const getUrls = () => {
       .then(data => data.urls)
 }
 
-export default getUrls;
+const addUrl = (newUrl) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUrl)
+    })
+}
+export {getUrls};
