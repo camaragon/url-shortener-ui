02ea8526124db9875944380856c1c152245b77b2
Cameron Aragon <caragon4695@gmail.com>
2021-03-09 10:33:12 -0700

    Test the contents of each div

--- cypress/integration/initial-display_spec.js
+++ cypress/integration/initial-display_spec.js
@@ -26,9 +26,23 @@ describe('URL Shortener', () => {
         .get('section').children().its('length').should('eq', 2);
     });
 
-    it('Should display the existing shortened URLs', () => {
+    it('Should display the name, long URL and short URL in each shortened URL container', () => {
         cy
         .wait(500)
-        .get('section').children().its('length').should('eq', 2);
+        .get('section').within(() => {
+            cy
+            .get('.url:first').within(() => {
+                cy
+                .get('h3').contains('This is the Final Assesment Mod 3 Turing')
+                .get('a').contains('http://localhost:3001/useshorturl/1')
+                .get('p').contains('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+            })
+            .get('.url:last').within(() => {
+                cy
+                .get('h3').contains('Why is Apple\'s URL so long?')
+                .get('a').contains('https://www.apple.com/2')
+                .get('p').contains('https://www.apple.com/?afid=p238%7CseIEs444j-dc_mtid_1870765e38482_pcrid_490032927326_pgrid_13945964887_&cid=aos-us-kwgo-brand-apple--slid---product-')
+            })
+        })
     });
 })
